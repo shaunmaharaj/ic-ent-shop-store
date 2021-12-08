e034cdf4fa62e4387782e3fa9ea7a1b9cd3f97f3
Shaun Maharaj <39800563+shaunmaharaj@users.noreply.github.com>
2021-12-08 17:27:02 -0500

    Shaunmaharaj/additional cms components (#6)
    * Add additional components
    
    * additional Netlify CMS component cleanup

--- cms/config.js
+++ cms/config.js
@@ -30,7 +30,71 @@ export default {
               {
                 label: 'Hero Image',
                 name: 'hero_image',
+                widget: 'file'
+              },
+            ],
+          },
+          {
+            label: 'Three Grid Container',
+            name: 'three grid container',
+            file: 'content/pages/3_grid_container.md',
+            fields: [
+              {
+                label: 'Title',
+                name: 'title',
+                widget: 'string',
+              },
+              {
+                label: 'Description 1',
+                name: 'description1',
+                widget: 'markdown',
+              },
+              {
+                label: 'Image 1',
+                name: 'image1',
                 widget: 'image'
+              },
+              {
+                label: 'Description 2',
+                name: 'description2',
+                widget: 'markdown',
+              },
+              {
+                label: 'Image 2',
+                name: 'image2',
+                widget: 'image'
+              },
+              {
+                label: 'Description 3',
+                name: 'description3',
+                widget: 'markdown',
+              },
+              {
+                label: 'Image 3',
+                name: 'image3',
+                widget: 'image'
+              },
+            ],
+          },
+          {
+            label: 'Secondary Home',
+            name: 'secondaryhome',
+            file: 'content/pages/secondary_home.md',
+            fields: [
+              {
+                label: 'Secondary Hero Title',
+                name: 'secondary_hero_title',
+                widget: 'string',
+              },
+              {
+                label: 'Secondary Hero Description',
+                name: 'secondary_hero_description',
+                widget: 'markdown',
+              },
+              {
+                label: 'Secondary Hero Image',
+                name: 'secondary_hero_image',
+                widget: 'file'
               },
             ],
           },
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,88 @@
+// @ts-nocheck
 import { NextPage, GetStaticProps } from 'next';
 interface Props {
-  content: { attributes: HomeAttributes };
+  hero_spot_content: { attributes: HomeAttributes };
+  three_grid_content: { attributes: HomeAttributesThreeGrid };
+  secondary_home_content: { attributes: HomeAttributesSecondary };
 }
 interface HomeAttributes {
   hero_title: string;
   hero_description: string;
   hero_image: string;
 }
-const HomePage: NextPage<Props> = ({ content }) => {
-  const { attributes } = content;
-return (
+interface HomeAttributesThreeGrid {
+  title: string;
+  description1: string;
+  image1: string;
+  description2: string;
+  image2: string;
+  description3: string;
+  image3: string
+}
+interface HomeAttributesSecondary {
+  secondary_hero_title: string;
+  secondary_hero_description: string;
+  secondary_hero_image: string;
+}
+const HomePage: NextPage<Props> = ({ hero_spot_content, three_grid_content, secondary_home_content }) => {
+  return (
     <>
-      <h1>{attributes.hero_title}</h1>
-      <p>{attributes.hero_description}</p>
-      <img src={attributes.hero_image} alt='hero image' />
+      <p>Header Placement</p>
+      {/* <img src={attributes.hero_image} alt='hero image' /> */}
+      <p>Content Placement</p>
+      <div id="content">
+        <div className="heroBanner">
+          <div>
+            <div>
+              <a className="heroBanner_link" href="/shop/categories/27">
+                <video src={hero_spot_content.attributes.hero_image} alt='hero image' className="heroBanner_video" loop="true" autoplay="true" muted></video>
+              </a>
+              <div class="overlay">
+                <p>bring good food for you!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="homepageGrid" >
+          <div>
+            <h3>Delicious Holiday Favorites</h3>
+            <div>
+              <img src={three_grid_content.attributes.image1} alt='hero image' className="heroBanners_image"></img>
+              <span className="subtitle">Holiday Appetizers</span>
+            </div>
+            <div>
+              <img src={three_grid_content.attributes.image2} alt='hero image' className="heroBanners_image"></img>
+              <span className="subtitle">Holiday Sides</span>
+            </div>
+            <div>
+              <img src={three_grid_content.attributes.image3} alt='hero image' className="heroBanners_image"></img>
+              <span className="subtitle">Desserts</span>
+            </div >
+          </div >
+        </div>
+        <div className="heroBanner">
+          <div>
+            <div>
+              <a className="heroBanner_link" href="/shop/categories/27">
+                <img src={secondary_home_content.attributes.secondary_hero_image} alt='hero image' className="heroBanner_image"></img>
+              </a>
+            </div>
+          </div>
+        </div>
+        <unata-shaun>hehehe</unata-shaun>
+        <div dangerouslySetInnerHTML={createMarkup()} />;
+      </div >
+      <p>Footer Placement</p>
     </>
   );
 };
+function createMarkup() {
+  return { __html: '<div><div><input type="text"/></div></div>' };
+}
 export const getStaticProps: GetStaticProps = async () => {
-  const content = await import(`../content/pages/${'home'}.md`);
-  return { props: { content: content.default } };
+  const hero_spot_content = await import(`../content/pages/${'home'}.md`);
+  const three_grid_content = await import(`../content/pages/${'3_grid_container'}.md`);
+  const secondary_home_content = await import(`../content/pages/${'secondary_home'}.md`);
+  return { props: { hero_spot_content: hero_spot_content.default, three_grid_content: three_grid_content.default, secondary_home_content: secondary_home_content.default } };
 };
 export default HomePage;
