ec8ee8666f064ad6f3ac193874d6047b6b43e3eb
Shaun Maharaj <shaun.maharaj@instacart.com>
2021-12-08 17:20:16 -0500

    additional Netlify CMS component cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 // @ts-nocheck
 import { NextPage, GetStaticProps } from 'next';
 interface Props {
-content: { attributes: HomeAttributes };
+  hero_spot_content: { attributes: HomeAttributes };
+  three_grid_content: { attributes: HomeAttributesThreeGrid };
+  secondary_home_content: { attributes: HomeAttributesSecondary };
 }
 interface HomeAttributes {
   hero_title: string;
   hero_description: string;
   hero_image: string;
-  secondary_hero_title: string;
-  secondary_hero_description: string;
-  secondary_hero_image: string;
+}
+interface HomeAttributesThreeGrid {
   title: string;
   description1: string;
   image1: string;
@@ -18,8 +19,12 @@ interface HomeAttributes {
   description3: string;
   image3: string
 }
-const HomePage: NextPage<Props> = ({ content }) => {
-  const { attributes } = content;
+interface HomeAttributesSecondary {
+  secondary_hero_title: string;
+  secondary_hero_description: string;
+  secondary_hero_image: string;
+}
+const HomePage: NextPage<Props> = ({ hero_spot_content, three_grid_content, secondary_home_content }) => {
   return (
     <>
       <p>Header Placement</p>
@@ -30,32 +35,43 @@ const HomePage: NextPage<Props> = ({ content }) => {
           <div>
             <div>
               <a className="heroBanner_link" href="/shop/categories/27">
-                <video src={attributes.hero_image} alt='hero image' className="heroBanner_image"></video>
+                <video src={hero_spot_content.attributes.hero_image} alt='hero image' className="heroBanner_video" loop="true" autoplay="true" muted></video>
               </a>
+              <div class="overlay">
+                <p>bring good food for you!</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="heroBanners">
+        <div className="homepageGrid" >
           <div>
+            <h3>Delicious Holiday Favorites</h3>
             <div>
-              <a className="heroBanners_link" href="/shop/categories/27">
-                <img src={attributes.secondary_hero_image} alt='hero image' className="heroBanners_image"></img>
-              </a>
+              <img src={three_grid_content.attributes.image1} alt='hero image' className="heroBanners_image"></img>
+              <span className="subtitle">Holiday Appetizers</span>
             </div>
-          </div>
+            <div>
+              <img src={three_grid_content.attributes.image2} alt='hero image' className="heroBanners_image"></img>
+              <span className="subtitle">Holiday Sides</span>
+            </div>
+            <div>
+              <img src={three_grid_content.attributes.image3} alt='hero image' className="heroBanners_image"></img>
+              <span className="subtitle">Desserts</span>
+            </div >
+          </div >
         </div>
-        <div className="heroBanners">
+        <div className="heroBanner">
           <div>
             <div>
-              <a className="heroBanners_link" href="/shop/categories/27">
-                <img src={attributes.secondary_hero_image} alt='hero image' className="heroBanners_image"></img>
+              <a className="heroBanner_link" href="/shop/categories/27">
+                <img src={secondary_home_content.attributes.secondary_hero_image} alt='hero image' className="heroBanner_image"></img>
               </a>
             </div>
           </div>
         </div>
         <unata-shaun>hehehe</unata-shaun>
         <div dangerouslySetInnerHTML={createMarkup()} />;
-      </div>
+      </div >
       <p>Footer Placement</p>
     </>
   );
@@ -64,7 +80,9 @@ function createMarkup() {
   return { __html: '<div><div><input type="text"/></div></div>' };
 }
 export const getStaticProps: GetStaticProps = async () => {
-  const content = await import(`../content/pages/${'home'}.md`);
-  return { props: { content: content.default } };
+  const hero_spot_content = await import(`../content/pages/${'home'}.md`);
+  const three_grid_content = await import(`../content/pages/${'3_grid_container'}.md`);
+  const secondary_home_content = await import(`../content/pages/${'secondary_home'}.md`);
+  return { props: { hero_spot_content: hero_spot_content.default, three_grid_content: three_grid_content.default, secondary_home_content: secondary_home_content.default } };
 };
 export default HomePage;
