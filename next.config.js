0f9dd4a1f757b568793ea31503b56c123cb619aa
Shaun Maharaj <shaun.maharaj@instacart.com>
2021-12-08 11:49:11 -0500

    Add cms support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
-}
+  pageExtensions: ["tsx"],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          type: "json",
+          use: "yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+      ]
+    );
+    return config;
+  },
+};
